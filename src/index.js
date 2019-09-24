653cd2e903f5a5f4b0ca16fa1983dda853f8def7
caninax <cani_nax@yahoo.com>
2019-09-24 22:09:23 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    const brStrArr = bracketsConfig.map(item => item.join(''));
+    let k = Math.floor(str.length / 2);
+    while (k) {
+        brStrArr.map(item => str.indexOf(item) !== -1 ? str = str.replace(item, "") : str);
+        k--;
+    }
+    return str === "" ? true : false;
 }
